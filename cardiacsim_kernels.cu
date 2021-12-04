b34391d572c82f592303c7918b7319e177fba64e
ssrdar18 <ssrdar18@ai12.kuacc.ku.edu.tr>
2021-12-04 17:14:15 +0300

    Implement non-functional kernel 4 (:

--- cardiacsim_kernels.cu
+++ cardiacsim_kernels.cu
@@ -2,6 +2,7 @@
 #include <assert.h>
 
 #define BLOCK_SIZE 16
+#define TWO 2
 
 double *d_E, *d_R, *d_E_prev;
 
@@ -94,6 +95,58 @@ __global__ void v3_kernel(double *E, double *E_prev, double *R,
 	}
 }
 
+__global__ void v4_kernel(double *E, double *E_prev, double *R,
+	const double alpha, const int n, const int m, const double kk,
+	const double dt, const double a, const double epsilon,
+	const double M1, const double M2, const double b)
+{
+	// const int tmpSize = BLOCK_SIZE*BLOCK_SIZE + (TWO * m) + (n * TWO);
+	__shared__ int shared_E[1056];
+	__shared__ int shared_E_prev[1056];
+	__shared__ int shared_R[1056];
+
+	int l_col = threadIdx.x + 1;
+	int l_row = threadIdx.y + 1;
+	int lwidth = BLOCK_SIZE + 2;
+	int lindex = l_row * lwidth + l_col;
+
+	int col = blockIdx.x * blockDim.x + threadIdx.x + 1;
+	int row = blockIdx.y * blockDim.y + threadIdx.y + 1;
+	int gwidth = m + 2;
+	int gindex = row * gwidth + col;
+	
+	shared_E[lindex] = E[gindex];
+	shared_E_prev[lindex] = E_prev[gindex];
+	shared_R[lindex] = R[gindex];
+
+	if (threadIdx.x < 1) {
+		shared_E_prev[lindex - 1] = E_prev[gindex - 1];
+		shared_E_prev[lindex + BLOCK_SIZE] = E_prev[gindex + BLOCK_SIZE];
+	}
+
+	if (threadIdx.y < 1) {
+		shared_E_prev[lindex - lwidth] = E_prev[gindex - gwidth];
+		shared_E_prev[lindex + lwidth*m] = E_prev[gindex + gwidth*m];
+	}
+		
+	__syncthreads();
+
+	if (row <= m && col <= n)
+	{
+		shared_E[lindex] = shared_E_prev[lindex] + alpha * (shared_E_prev[lindex + 1] + shared_E_prev[lindex - 1] - 4 * shared_E_prev[lindex] + shared_E_prev[lindex + lwidth] + shared_E_prev[lindex - lwidth]);
+
+		__syncthreads();
+
+		double e_current = shared_E[lindex];
+		double r_current = shared_R[lindex];
+
+		shared_E[lindex] = e_current - dt * (kk * e_current * (e_current - a) * (e_current - 1) + e_current * r_current);
+		e_current = shared_E[lindex];
+
+		shared_R[lindex] = r_current + dt * (epsilon + M1 * r_current / (e_current + M2)) * (-r_current - kk * e_current * (e_current - b - 1));
+	}
+}
+
 void deviceKernel(double **E, double **E_prev, double **R, const double alpha, const int n, const int m, const double kk,
 	const double dt, const double a, const double epsilon, const double M1, const double M2, const double b, int shouldMalloc, int shouldFree, int v){
 	
@@ -130,9 +183,11 @@ void deviceKernel(double **E, double **E_prev, double **R, const double alpha, c
 		v3_kernel<<<grid, block>>>(d_E, d_E_prev, d_R, alpha, n, m, kk, dt, a, epsilon, M1, M2, b);
 		cudaDeviceSynchronize();
 	}else{
-		
+		v4_kernel<<<grid, block>>>(d_E, d_E_prev, d_R, alpha, n, m, kk, dt, a, epsilon, M1, M2, b);
+		cudaDeviceSynchronize();
 	}
 	
+	// printf("%d \n", BLOCK_SIZE*BLOCK_SIZE + (2 * m) + (n * 2));
 	cudaMemcpy(E + copyOffset, d_E, matSize, cudaMemcpyDeviceToHost);
 	cudaMemcpy(E_prev + copyOffset, d_E_prev, matSize, cudaMemcpyDeviceToHost);
 	if (shouldFree)
